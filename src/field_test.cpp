6c9e2fef027ab6ad55457f6d0e48d3a8da666ca2
Enrico Benedetti <enrico_benedetti@yahoo.com>
2012-09-15 20:34:30 +0200

    changed field_test

--- src/field_test.cpp
+++ src/field_test.cpp
@@ -15,7 +15,10 @@ int main(int argc, char** argv) {
 
     point_t p3 = {-1, -1};
     assert(!field.contains(p3));
-    
+
+    point_t p4 = {100, 100};
+    assert(!field.contains(p4));
+
     /* test is_occupied */ 
     
     shape_t r1 = {{0, 0}, {10, 10}};
@@ -24,11 +27,11 @@ int main(int argc, char** argv) {
     shape_t r2 = {{20, 20}, {10, 10}};
     field.add(&r2);
     
-    point_t p4 = {5, 5};
-    assert(field.is_occupied(p4));
+    point_t p5 = {5, 5};
+    assert(field.is_occupied(p5));
     
-    point_t p5 = {15, 15};
-    assert(!field.is_occupied(p5));
+    point_t p6 = {15, 15};
+    assert(!field.is_occupied(p6));
     
     return 0;
 }
